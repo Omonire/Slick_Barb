e9110ad4e9411cac7e45b0080d5162b9d4594883
omonire <omonire4@gmail.com>
2026-09-09 22:58:19 +0100

    Fix routing: switch to BrowserRouter for clean /admin URLs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { AdminLogin } from './components/AdminLogin'
@@ -8,13 +8,13 @@ import { AdminDashboard } from './components/AdminDashboard'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin" element={<AdminLogin />} />
         <Route path="*" element={<App />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
